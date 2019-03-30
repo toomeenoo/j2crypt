7e1724cac9b9fecf814576f691098a200636b593
Tomáš Molinari <tomie.molinari@gmail.com>
2019-03-30 22:09:13 +0100

    Added php documentation

--- j2crypt-php.php
+++ j2crypt-php.php
@@ -1,14 +1,13 @@
 <?php
-/*
-*
-* j2crypt-php.php
-* 
-* @description J2crypt Library - 2Way 2D+ data crypting
-* @version 1.3
-* @author Tomáš Molinari @toomeenoo <tomie.molinari@gmail.com>
-* @license MIT
-*
-*/
+/**
+ * j2crypt-php.php
+ * 
+ * @description J2crypt Library - 2Way 2D+ data crypting
+ * @link https://github.com/toomeenoo/j2crypt
+ * @version 1.3
+ * @author Tomáš Molinari @toomeenoo <tomie.molinari@gmail.com>
+ * @license MIT
+ */
 class J2crypt
 {
 	private $map;
@@ -19,7 +18,6 @@ class J2crypt
 	private $data;
 	private $precomputed = false;
 	private $precomp_bin7;
-	public $duration = 0;	
 	
 	public function __construct(){
 		// For old php versions: 
@@ -30,15 +28,27 @@ class J2crypt
 		}
 		return $this;
 	}
-
-    public function toFile($filename) {
+	
+	/**
+	 * Writes crypted output to file
+	 * @param string $filename filesystem path and filename to be saved 
+	 * @return J2crypt self object
+	 */
+  public function toFile($filename) {
 		file_put_contents($filename, "");
 		$output_real = $this->getCrypted("raw");
 		$fil = fopen($filename,"w+");
 		fwrite($fil, $output_real);
-    }
+		return $this;
+  }
 	
-	public function directDownload($filename = "crypted.tcd", $die = false) {
+	/**
+	 * Downloads crypted output
+	 * @param string $filename = "crypted.j2cd" filename to output
+	 * @param boolean $die = false exit script on output
+	 * @return J2crypt self object
+	 */
+	public function directDownload($filename = "crypted.j2cd", $die = false) {
 		header('Content-Type: application/j2crypt-data');
 		header('Content-Disposition: attachment; filename="'.$filename.'"');
 		if($die){
@@ -46,8 +56,17 @@ class J2crypt
 		}else{
 			echo $this->getCrypted("raw");
 		}
+		return $this;
   }
 	
+	/**
+	 * Locks provided data
+	 * set new data to precomputed
+	 * set new data to precomputed_bin7
+	 * 
+	 * @param int|NULL $datalen = NULL length of data to be locked
+	 * @return int[] crypted output
+	 */
 	private function lock($datalen = NULL){
 		$this->lockmap = "";
 		$data = $this->data;
@@ -63,11 +82,11 @@ class J2crypt
 		while($i < $datalen /* && $r < $height*/){
 			$c = 0;
 			while($i < $datalen && $c < $width){
-				$mc = $c % $this->mapSize[0];
-				$mr = $r % $this->mapSize[1];
-				$mv = $this->map[$mc+$mr*$this->mapSize[0]];
-				$ps = ord($this->password[($i+$magicUnknown)%$this->passwordSize]);
-				$f = (ord($data[$i])+ord($mv)+$ps)%128;
+				$mc = $c % $this->mapSize[0];//Map rows
+				$mr = $r % $this->mapSize[1];//Map colums
+				$mv = $this->map[$mc+$mr*$this->mapSize[0]];//Map value
+				$ps = ord($this->password[($i+$magicUnknown)%$this->passwordSize]);//Password number
+				$f = (ord($data[$i])+ord($mv)+$ps)%128; //final number
 				$output[] = $f;
 				$bin7 .= sprintf("%'.07b",$f);
 				$c++;
@@ -80,12 +99,20 @@ class J2crypt
 		return $output;
 	}
 	
+	/**
+	 * Getter for locked data
+	 * @return int[] locked data
+	 */
 	private function locked(){
 		return ($this->precomputed ? $this->precomputed : $this->lock());
 	}
 
+	/**
+	 * Getter for locked and packed data
+	 * Output format: binary(7bit) joined and packed to string
+	 * @return string packed locked data
+	 */
 	private function getRaw(){
-		$start = microtime();
 		$out = $this->locked();
 		$output_real = $this->precomp_bin7;
 		$remaining = strlen($output_real);
@@ -101,9 +128,14 @@ class J2crypt
 			}
 			$i++;
 		}
-		$this->duration = microtime() - $start;
 		return $output_raw;
 	}
+
+	/**
+	 * Getter for locked and packed data
+	 * Output format: binary(7bit) join to binary(8bit) to int
+	 * @return int[] locked data
+	 */
 	private function getPacked(){
 		$output_real = "";
 		$out = $this->locked();
@@ -126,11 +158,26 @@ class J2crypt
 		return $output_raw;
 	}
 	
+	/**
+	 * Opens file and unlock contents
+	 * @return string Unlocked data
+	 */
 	public function unlockFile($filename){
 		$content = file_get_contents($filename);
 		return $this->unlock($content);
 	}
 
+	/**
+	 * Get locked data in different formats: <br>
+	 * - 'raw' (default) = binary(7bit) joined and packed to string <br>
+	 * - 'base64' = raw packed to base64 <br>
+	 * - 'bin7' = string of binary(7bit) <br>
+	 * - 'dec7' = string of numbers 0-127 delimited with spaces <br>
+	 * - 'dec8' = string of numbers 0-255 delimited with spaces<br>
+	 * 
+	 * @param string $format
+	 * @return string requested ouptut
+	 */
 	public function getCrypted($format = "raw"){
 		$out = $this->locked();
 		if($format == "raw" || $format == "base64"){
@@ -151,13 +198,23 @@ class J2crypt
 		return $output_real;
 	}
 	
+	/**
+	 * Setter for data to be locked
+	 * @param string $data input data
+	 * @return \J2crypt this object
+	 */
 	public function setData($data) {
-        $this->data = $data;
+    $this->data = $data;
 		$this->dataSize = $this->rectFrom(strlen($data));
 		$this->precomputed = false;
 		return $this;
   }
 
+	/**
+	 * Unlocks data wich are passed in argumetn
+	 * @param string $content Input locked data
+	 * @return string Unlocked, output data
+	 */
 	public function unlock($content){
 		$i = 0;
 		$len = strlen($content);
@@ -202,21 +259,38 @@ class J2crypt
 		}
 		return $output;
 	}
-	
-	public function setPwd($pwd, $reGenerateMap = true) {
+
+	/**
+	 * Sets password to be used in this object
+	 * @param string $pwd Password
+	 * @return \J2crypt this object
+	 */
+	public function setPwd($pwd) {
 		$this->password = $pwd;
 		$this->passwordSize = strlen($pwd);
 		$this->precomputed = false;
-		if($reGenerateMap)
-			$this->generateMap();
 		return $this;
-    }
-	public function setMap($map_data, $sizeX, $sizeY) {
+	}
+
+	/**
+	 * Set binary string and size as used map
+	 * @param string $map_data Binary string of map values
+	 * @param int $sizeX width of map (up to 127)
+	 * @param int $sizeY height of map (unlimited)
+	 * @return \J2crypt this object
+	 */
+	private function setMap($map_data, $sizeX, $sizeY) {
 		$this->map = $map_data;
 		$this->mapSize = array($sizeX, $sizeY);
 		$this->precomputed = false;
 		return $this;
-    }
+	}
+	
+	/**
+	 * Set json map as used map
+	 * @param string $json_text Json map
+	 * @return \J2crypt this object
+	 */
 	public function setMapJson($json_text) {
 		$obj = json_decode ($json_text, true);
 		$len = strlen($obj["dataHex"]);
@@ -229,6 +303,13 @@ class J2crypt
 		return $this->setMap($raw_map_data, intval($obj["width"]), intval($obj["height"]));
 	}
 	
+
+	/**
+	 * Function to create "natural rectangle" from given square size
+	 * @param int $int data size
+	 * @param int $aLimit limit of width (still 127)
+	 * @return array [width, height, totalSize]
+	 */
 	private function rectFrom($int, $aLimit = 127){
 		$dataLen = $int;
 		$b = $dataLen;
@@ -246,16 +327,27 @@ class J2crypt
 		if((($a*$n)<$aLimit) && $a < $b/3){
 			return $this->rectFrom($int+1);
 		}
-
 		return array($a, $b,  $int);
 	}
 	
+	/**
+	 * Strictly generate new map from password
+	 * @todo EXPERIMENTAL, not implemented on javascript side 
+	 * @param int $width Map's width
+	 * @param int $height Map's height
+	 * @return \J2crypt this object
+	 */
 	private function generateMap($width = 6, $height = 6){
 		$this->mapSize = array($width, $height);
 		$this->map = sha1($this->password, true).md5($this->password, true);
 		$this->precomputed = false;
+		return $this;
 	}
 
+	/**
+	 * Get current map in json format to be sent or stored
+	 * @return string Json map
+	 */
 	public function getMap(){
 		$ret = '{"width":'.$this->mapSize[0].',"height":'.$this->mapSize[1].',"dataHex":"';
 		$i = 0;
@@ -269,6 +361,13 @@ class J2crypt
 		}
 		return $ret.'"}';
 	}
+
+	/**
+	 * Randomly generate new key map of given size
+	 * @param int $x width (bigger than 127 is useless)
+	 * @param int $y height (any size should be used)
+	 * @return \J2crypt this object
+	 */
 	public function customMap($x, $y){
 		$md = '';
 		$i = 0;
